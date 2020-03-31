101425bd3c07efb0bb333de41335ee2cd2868692
Jhon Arias <jhonarmont@hotmail.com>
2020-03-31 10:10:12 -0500

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,12 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+movie = Movie.create(
+  name: "Star Wars",
+  description: "aliens and humans with ligth swords",
+  image_url: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/04/30/18/pg-46-twitter-past-1-getty.jpg",
+  start_date: Date.new(2020, 6, 2),
+  end_date: Date.new(2020, 6, 9),
+)
+Reservation.create(name: "Jhon Arias", email: "jhonarmont@hotmail.com", cedula: "10000000", movie_id: 1)
